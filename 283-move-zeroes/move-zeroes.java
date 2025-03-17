3de83d3cc3b93a72ce97f59a4996372526b1907e
SUBH0007 <151958170+SUBH0007@users.noreply.github.com>
2025-03-18 00:13:06 +0530

    Time: 4 ms (14.53%) | Memory: 45.8 MB (90.69%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void moveZeroes(int[] a) {
+        int n=a.length;
+        ArrayList<Integer> temp = new ArrayList<>();
+        //copy non-zero elements
+        //from original -> temp array:
+        for (int i = 0; i < n; i++) {
+            if (a[i] != 0)
+                temp.add(a[i]);
+        }
+
+        // number of non-zero elements.
+        int nz = temp.size();
+
+        //copy elements from temp
+        //fill first nz fields of
+        //original array:
+        for (int i = 0; i < nz; i++) {
+            a[i] = temp.get(i);
+        }
+
+        //fill rest of the cells with 0:
+        for (int i = nz; i < n; i++) {
+            a[i] = 0;
+        }
+        }
+ }
