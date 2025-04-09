472411e1259b5d2073011327f0b57cd6ba170f74
SUBH0007 <151958170+SUBH0007@users.noreply.github.com>
2025-04-09 19:27:13 +0530

    Time: 3 ms (52.54%) | Memory: 44.5 MB (55.59%) - LeetSync

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+         Arrays.sort(nums);
+       int res = 0;
+       int i = 0;
+       while(i<nums.length){
+            res++;
+            int temp = nums[i];
+            while(i < nums.length && temp == nums[i]){
+                i++;
+            }
+       }
+
+       
+       if(nums[0] == k){
+            return res-1;
+       }
+
+       if(nums[0] > k){
+            return res;
+       }
+
+       return -1;
+    }
+}
