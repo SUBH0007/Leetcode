4952f4fe30766215925cbb02151c2edd48b9b06b
Subhadeep Haldar <151958170+SUBH0007@users.noreply.github.com>
2025-05-25 18:34:09 +0530

    Time: 56 ms (74.19%) | Memory: 58.6 MB (43.90%) - LeetSync

---
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int longestPalindrome(String[] words) {
+        Map<String, Integer> mpp = new HashMap<>();
+        for (var w : words) {
+            mpp.put(w, mpp.getOrDefault(w, 0) + 1);
+        }
+
+        int totalLength = 0;
+        int flag = 0;
+
+        for (var x : mpp.entrySet()) {
+            String word = x.getKey();
+            int freq = x.getValue();
+
+            String rev = new StringBuilder(word).reverse().toString();
+
+            if (word.equals(rev)) {
+                totalLength += (freq / 2) * 4;
+
+                if (freq % 2 == 1) {
+                    flag = 1;
+                }
+            } else {
+                if (word.compareTo(rev) < 0 && mpp.containsKey(rev)) {
+                    int freqOfReversed = mpp.get(rev);
+                    int numberOfPairs = Math.min(freq, freqOfReversed);
+
+                    totalLength += numberOfPairs * 4;
+                }
+            }
+        }
+
+        return totalLength + flag * 2;
+    }
+}
