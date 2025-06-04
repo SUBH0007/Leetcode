5de6d76414eacafeaf294b3b985ccc5ab0b4a91f
Subhadeep Haldar <151958170+SUBH0007@users.noreply.github.com>
2025-06-04 23:35:40 +0530

    Time: 7 ms (65.15%) | Memory: 47.4 MB (97.51%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,40 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-         if(strs == null || strs.length ==  0){
-            return new ArrayList<>();
-        }
+        HashMap<String,List<String>> ans=new HashMap<>();
+        for(String s:strs){
+            char[] chars=s.toCharArray();
+            Arrays.sort(chars);
+            String key=new String(chars);
 
-        Map<String, List<String>> frequencyStringMap = new HashMap<>();
-        for(String str : strs){
-
-            String frequencyString = getFrequencyString(str);
-            
-            if(frequencyStringMap.containsKey(frequencyString)){
-                frequencyStringMap.get(frequencyString).add(str);
-            }else{
-                List<String> strList = new ArrayList<>();
-                strList.add(str);
-                frequencyStringMap.put(frequencyString,strList);
+            if(!ans.containsKey(key)){
+                ans.put(key,new ArrayList<>());
             }
+            ans.get(key).add(s);
         }
-        return new ArrayList<>(frequencyStringMap.values());
-    }
-
-    static String getFrequencyString(String str){
-        int freq[] = new int[26];
-
-        for(char c: str.toCharArray()){
-            freq[c - 'a']++;
-        }
-
-        StringBuilder frequencyString = new StringBuilder("");
-        char c = 'a';
-        for(int i : freq){
-            frequencyString.append(c);
-            frequencyString.append(i);
-            c++;
-        }
-
-        return frequencyString.toString();
+        return new ArrayList<>(ans.values());
     }
 }
