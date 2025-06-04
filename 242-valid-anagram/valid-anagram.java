551b6ef34e5bf6f27a6656ee6548255655d3d5c9
Subhadeep Haldar <151958170+SUBH0007@users.noreply.github.com>
2025-06-04 22:24:46 +0530

    Time: 6 ms (45.45%) | Memory: 43.2 MB (77.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,13 +3,27 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        HashMap<Character, Integer> counts=new HashMap<>();
-        HashMap<Character, Integer> countt=new HashMap<>();
+        // HashMap<Character, Integer> counts=new HashMap<>();
+        // HashMap<Character, Integer> countt=new HashMap<>();
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     counts.put(s.charAt(i),counts.getOrDefault(s.charAt(i),0)+1);
+        //     countt.put(t.charAt(i),countt.getOrDefault(t.charAt(i),0)+1);
+        // }
+        // return counts.equals(countt);
+        int[] count = new int[26];
         for(int i=0;i<s.length();i++)
         {
-            counts.put(s.charAt(i),counts.getOrDefault(s.charAt(i),0)+1);
-            countt.put(t.charAt(i),countt.getOrDefault(t.charAt(i),0)+1);
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
         }
-        return counts.equals(countt);
+        for(int val:count)
+        {
+            if(val!=0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
