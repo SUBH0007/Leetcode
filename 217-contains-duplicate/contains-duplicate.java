99084078755224a1b40bb38795054bf994d7768e
Subhadeep Haldar <151958170+SUBH0007@users.noreply.github.com>
2025-06-04 21:39:46 +0530

    Time: 19 ms (34.66%) | Memory: 58.3 MB (59.78%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) 
     {
-        Arrays.sort(nums);
-        int n = nums.length;
-        for (int i = 1; i < n; i++) 
-        {
-            if (nums[i] == nums[i - 1])
-                return true;
-        }
-        return false;
+       Arrays.sort(nums);
+       for(int a=1;a<nums.length;a++)
+       {
+            
+                if(nums[a]==nums[a-1])
+                {
+                    return true;
+                }
+            
+       }
+       return false;
     }
 }
