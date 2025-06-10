a640e9a30835b533268c59bd303bab567fdd233c
Subhadeep Haldar <151958170+SUBH0007@users.noreply.github.com>
2025-06-10 20:08:50 +0530

    Time: 3 ms (22.42%) | Memory: 55.6 MB (51.17%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -4,19 +4,13 @@ class Solution {
         int pre[] = new int[n];
         pre[0] = 1;
         int s = 1;
-        
         for(int i = 1; i < n; i++) {
             pre[i] = pre[i - 1] * nums[i - 1];
         }
         for(int i = n - 1; i >= 0; i--) {
-            pre[i] = pre[i] * s;
-            s=s*nums[i];
+            pre[i] *= s;
+            s*=nums[i];
         }
-        
-        // int ans[] = new int[n];
-        // for(int i = 0; i < n; i++) {
-        //     ans[i] = pre[i] * suff[i];
-        // }
         return pre;
     }
 }
