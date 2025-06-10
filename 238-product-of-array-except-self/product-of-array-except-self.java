d706a12fab2df37737dd1626b5ba499e62ed0162
Subhadeep Haldar <151958170+SUBH0007@users.noreply.github.com>
2025-06-10 20:10:51 +0530

    Time: 2 ms (89.16%) | Memory: 55.3 MB (87.08%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,16 +1,29 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int pre[] = new int[n];
-        pre[0] = 1;
-        int s = 1;
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] * nums[i - 1];
+        // int n = nums.length;
+        // int pre[] = new int[n];
+        // pre[0] = 1;
+        // int s = 1;
+        // for(int i = 1; i < n; i++) {
+        //     pre[i] = pre[i - 1] * nums[i - 1];
+        // }
+        // for(int i = n - 1; i >= 0; i--) {
+        //     pre[i] *= s;
+        //     s*=nums[i];
+        // }
+        // return pre;
+         int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
         }
-        for(int i = n - 1; i >= 0; i--) {
-            pre[i] *= s;
-            s*=nums[i];
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
         }
-        return pre;
+        return res;
     }
 }
