c255055e7d83b17b1fa34307bab513a09a0366b6
Subhadeep Haldar <151958170+SUBH0007@users.noreply.github.com>
2025-06-21 21:50:35 +0530

    Time: 4 ms (100.00%) | Memory: 45.8 MB (17.02%) - LeetSync

---
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumDeletions(String word, int k) {
+        int[] freq = new int[26];
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            freq[ch - 'a']++;
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<freq.length;i++){
+            if(freq[i] == 0) continue;
+            int freqEle = freq[i];
+            int max = freqEle + k;
+            int ans = 0;
+            for(int j=0;j<freq.length;j++){
+                if(freq[j] < freqEle) ans = ans + freq[j];
+                else if(freq[j] > max) ans = ans + freq[j] - max;
+            }
+            min = Math.min(min, ans);
+        }
+        return min;
+    }
+}
