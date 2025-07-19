03847ccba566618afcc237f205d1012da64776bc
Subhadeep Haldar <151958170+SUBH0007@users.noreply.github.com>
2025-07-19 14:06:52 +0530

    Time: 42 ms (70.51%) | Memory: 55.4 MB (74.41%) - LeetSync

---
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> result = new ArrayList<>();
+
+        result.add(folder[0]);
+
+        for(int i = 1; i < folder.length; i++){
+            String currFolder = folder[i];
+            String lastFolder = result.get(result.size() - 1);
+            lastFolder += "/";
+
+            if(!currFolder.startsWith(lastFolder)){
+                result.add(currFolder);
+            }
+        }
+        return result;
+    }
+}
