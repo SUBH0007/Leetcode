4216f9f6b5a96b2d1cfcb00622322d32be5e482b
Subhadeep Haldar <151958170+SUBH0007@users.noreply.github.com>
2025-07-31 22:03:28 +0530

    Time: 412 ms (6.97%) | Memory: 73.7 MB (22.54%) - LeetSync

---
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int subarrayBitwiseORs(int[] arr) {
+        Set<Integer> result = new HashSet<>();
+        Set<Integer> current = new HashSet<>();
+        
+        for (int num : arr) {
+            Set<Integer> next = new HashSet<>();
+            next.add(num);
+            for (int val : current) {
+                next.add(val | num);
+            }
+            result.addAll(next);
+            current = next;
+        }
+
+        return result.size();
+    }
+}
