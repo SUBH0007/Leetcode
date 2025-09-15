f382d14eb94810fe88f53a43189b1c7f46f9fc01
Subhadeep Haldar <151958170+SUBH0007@users.noreply.github.com>
2025-09-15 22:07:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (45.47%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -14,17 +14,49 @@
  * }
  */
 class Solution {
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    //     List<Integer> res=new ArrayList<>();
+    //     inorder(root,res);
+    //     return res;
+    // }
+    // private void inorder(TreeNode node,List<Integer> res){
+    //     if(node==null){
+    //         return;
+    //     }
+    //     inorder(node.left,res);
+    //     res.add(node.val);
+    //     inorder(node.right,res);
+    // }
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res=new ArrayList<>();
-        inorder(root,res);
-        return res;
-    }
-    private void inorder(TreeNode node,List<Integer> res){
-        if(node==null){
-            return;
+        List<Integer> result = new ArrayList<>();
+        TreeNode current = root;
+
+        while (current != null) {
+            if (current.left == null) {
+                // If there's no left child, visit the current node and move to the right
+                result.add(current.val);
+                current = current.right;
+            } else {
+                // Find the inorder predecessor of the current node
+                TreeNode predecessor = current.left;
+                while (predecessor.right != null && predecessor.right != current) {
+                    predecessor = predecessor.right;
+                }
+
+                if (predecessor.right == null) {
+                    // If the predecessor's right child is null, create a thread to the current node
+                    predecessor.right = current;
+                    current = current.left;
+                } else {
+                    // If the predecessor's right child points to the current node,
+                    // it means we've already visited the left subtree, so remove the thread,
+                    // visit the current node, and move to the right
+                    predecessor.right = null; // Remove the thread
+                    result.add(current.val);
+                    current = current.right;
+                }
+            }
         }
-        inorder(node.left,res);
-        res.add(node.val);
-        inorder(node.right,res);
+        return result;
     }
 }
